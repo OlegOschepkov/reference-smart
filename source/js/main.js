4bcf652f6487b46a036b6d6a4fe31faade3013d4
Олег Ощепков <o_oleg_o@inbox.ru>
2019-08-27 09:20:36 +0400

    Fix

--- source/js/main.js
+++ source/js/main.js
@@ -9,7 +9,7 @@
       scrolls[i].addEventListener('click', function (e) {
         e.preventDefault();
         // eslint-disable-next-line no-invalid-this
-        var target = this.getAttribute("href");
+        var target = this.getAttribute('href');
         var targetPos = document.querySelector(target).offsetTop;
         var startPos = window.pageYOffset;
         var distance = targetPos - startPos;
@@ -17,27 +17,28 @@
         var duration = 300;
 
         function animation(currentTime) {
-          if(startTime === null) {
+          if (startTime === null) {
             startTime = currentTime;
           }
           var timeElapsed = currentTime - startTime;
           var run = ease(timeElapsed, startPos, distance, duration);
           window.scrollTo(0, run);
-          if(timeElapsed < duration) {
-            requestAnimationFrame(animation)
+          if (timeElapsed < duration) {
+            requestAnimationFrame(animation);
           }
         }
-        requestAnimationFrame(animation)
-      })
+        requestAnimationFrame(animation);
+      });
     }
   }
 
-
-  function ease (t, b, c, d) {
-    t /= d/2;
-    if (t < 1) return c/2*t*t + b;
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) {
+      return c / 2 * t * t + b;
+    }
     t--;
-    return -c/2 * (t*(t-2) - 1) + b;
+    return -c / 2 * (t * (t - 2) - 1) + b;
   }
 
   smoothScroll();
@@ -220,57 +221,4 @@
     }
   }
 
-})();
-
-// smooth scroll
-(function() {
-  'use strict';
-  // Feature Test
-  if ( 'querySelector' in document && 'addEventListener' in window && Array.prototype.forEach ) {
-    // Function to animate the scroll
-    var smoothScroll = function (anchor, duration) {
-      // Calculate how far and how fast to scroll
-      var startLocation = window.pageYOffset;
-      var endLocation = anchor.offsetTop;
-      var distance = endLocation - startLocation;
-      var increments = distance/(duration/16);
-      var stopAnimation;
-      // Scroll the page by an increment, and check if it's time to stop
-      var animateScroll = function () {
-        window.scrollBy(0, increments);
-        stopAnimation();
-      };
-      // If scrolling down
-      if ( increments >= 0 ) {
-        // Stop animation when you reach the anchor OR the bottom of the page
-        stopAnimation = function () {
-          var travelled = window.pageYOffset;
-          if ( (travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight) ) {
-            clearInterval(runAnimation);
-          }
-        };
-      }
-      // Loop the animation function
-      var runAnimation = setInterval(animateScroll, 16);
-    };
-    // Define smooth scroll links
-    var scrollToggle = document.querySelectorAll('.scroll');
-    // For each smooth scroll link
-    [].forEach.call(scrollToggle, function (toggle) {
-      // When the smooth scroll link is clicked
-      toggle.addEventListener('click', function(e) {
-        // Prevent the default link behavior
-        e.preventDefault();
-        // Get anchor link and calculate distance from the top
-        var dataID = toggle.getAttribute('href');
-        var dataTarget = document.querySelector(dataID);
-        var dataSpeed = toggle.getAttribute('data-speed');
-        // If the anchor exists
-        if (dataTarget) {
-          // Scroll to the anchor
-          smoothScroll(dataTarget, dataSpeed || 500);
-        }
-      }, false);
-    });
-  }
 })();
